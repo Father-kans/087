a817c6137dcec95f9b380dd84e64f5cdb39118ba
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-08-15 12:40:56 +0900

    Update ui.cc

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -318,8 +318,7 @@ QUIState::QUIState(QObject *parent) : QObject(parent) {
   ui_state.sm = std::make_unique<SubMaster, const std::initializer_list<const char *>>({
     "modelV2", "controlsState", "liveCalibration", "radarState", "deviceState", "roadCameraState",
     "pandaState", "carParams", "driverMonitoringState", "sensorEvents", "carState", "liveLocationKalman",
-    "gpsLocationExternal", "roadCameraState",
-    "carControl", "liveParameters", "ubloxGnss"});
+    "gpsLocationExternal", "carControl", "liveParameters", "ubloxGnss"});
 
   ui_state.fb_w = vwp_w;
   ui_state.fb_h = vwp_h;
