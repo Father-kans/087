e95295cee79846cad42cd581a89ba231277001c2
SangJo Kim <45070398+KimSangJo@users.noreply.github.com>
2021-09-25 17:13:58 +0900

    Enable Oneplus3T to EON

--- common/api/__init__.py
+++ common/api/__init__.py
@@ -37,10 +37,12 @@ class Api():
     
 
 def api_get(endpoint, method='GET', timeout=None, access_token=None, **params):
+  backend = "https://api.commadotai.com/"
+
   headers = {}
   if access_token is not None:
     headers['Authorization'] = "JWT "+access_token
 
   headers['User-Agent'] = "openpilot-" + version
 
-  return requests.request(method, API_HOST+endpoint, timeout=timeout, headers=headers, params=params)
+  return requests.request(method, backend+endpoint, timeout=timeout, headers=headers, params=params)
